d62d95ebcfaf2729443546079060425aeed52e07
anonymous <coder1998man@gmail.com>
2024-01-15 17:18:51 +0330

    custom user admin defined

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    # Specify the fields to display in the admin list view
+    list_display = ['username', 'email', 'is_staff']
+
+    # Specify the fields to use for searching in the admin list view
+    search_fields = ['username', 'email']
+
+    # Specify the fields to filter the admin list view by
+    list_filter = ['is_staff', 'is_superuser']
+
+    # Specify the fieldsets for the admin detail view
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
