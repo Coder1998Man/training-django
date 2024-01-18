5acc9b00fb9cbe438a2bc59359f4dc424c5c618c
anonymous <coder1998man@gmail.com>
2024-01-18 16:15:44 +0330

    linked a title of projects and can get that full info...

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import projects
+from .views import projects, project
 
 urlpatterns = [
     path('', projects, name='home'),
+    path('project/<int:pk>/', project, name='project'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,31 +1,39 @@
 from django.shortcuts import render
 
+projects_list = [
+    {
+        'id': 1,
+        'title': 'Ecommerce Website',
+        'description': 'fully functional ecommerce website',
+    },
+    {
+        'id': 2,
+        'title': 'Portfolio Website',
+        'description': 'this was a project where i built my portfolio',
+    },
+    {
+        'id': 3,
+        'title': 'Social Network',
+        'description': 'Awsome open source project I am still working on',
+    }
+]
+
 
 def projects(request):
-    projectsList = [
-        {
-            'id': '1',
-            'title': 'Ecommerce Website',
-            'description': 'fully functional ecommerce website',
-        },
-        {
-            'id': '2',
-            'title': 'Portfolio Website',
-            'description': 'this was a project where i built my portfolio',
-        },
-        {
-            'id': '3',
-            'title': 'Social Network',
-            'description': 'Awsome open source project I am still working on',
-        }
-    ]
-
     context = {
-        'projects': projectsList
+        'projects': projects_list
     }
 
     return render(request, 'blog/projects.html', context)
 
 
-def project(request):
-    pass
+def project(request, pk):
+   project_obj = None
+   for i in projects_list:
+       if i['id'] == pk:
+           project_obj = i
+           break
+   context = {
+       'project': project_obj
+   }
+   return render(request, 'blog/project.html', context)
