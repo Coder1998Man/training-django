1d65db72d80fde3776ace621c52fbf06ad3f7c85
Coder1998Man <coder1998man@gmail.com>
2024-01-24 23:19:58 +0330

    new route for create new project

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import projects, project
+from .views import projects, project, create_project_form
 
 urlpatterns = [
     path('', projects, name='home'),
+    path('create_project_form/', create_project_form, name='create_project'),
     path('project/<uuid:pk>/', project, name='project'),
 ]
