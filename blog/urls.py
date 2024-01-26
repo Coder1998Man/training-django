fb8694aae24ce3fae9c830e3a00d52fb83b09ef7
Coder1998Man <coder1998man@gmail.com>
2024-01-26 20:17:19 +0330

    edit home path

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import projects, project, create_project_form
+from .views import projects, project, create_project
 
 urlpatterns = [
-    path('', projects, name='home'),
-    path('create_project_form/', create_project_form, name='create_project'),
+    path('', projects, name='projects'),
+    path('create_project_form/', create_project, name='create_project'),
     path('project/<uuid:pk>/', project, name='project'),
 ]
