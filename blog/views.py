3a277054f20a8fdd8b5c22b7dc321333abfdbc33
Coder1998Man <coder1998man@gmail.com>
2024-01-26 20:18:32 +0330

    write the create project logic

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Project
 from .forms import ProjectForm
 
@@ -19,6 +19,15 @@ def project(request, pk):
     return render(request, 'blog/project.html', context)
 
 
-def create_project_form(request):
+def create_project(request):
     form = ProjectForm()
+
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+
     return render(request, 'blog/create_project_form.html', {'form': form})
+
+
