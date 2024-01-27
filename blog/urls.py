a139450331e0b1e546b9675b2d57ac7dbdacb4d9
Coder1998Man <coder1998man@gmail.com>
2024-01-27 13:55:54 +0330

    added update / delete urls path

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import projects, project, create_project
+from .views import projects, project, create_project, update_project, delete_project
 
 urlpatterns = [
     path('', projects, name='projects'),
     path('create_project_form/', create_project, name='create_project'),
     path('project/<uuid:pk>/', project, name='project'),
+    path('update_project/<str:pk>/', update_project, name='update_project'),
+    path('delete_project/<str:pk>/', delete_project, name='delete_project'),
 ]
