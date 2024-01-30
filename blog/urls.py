45442884a4c8ad03fb0794afce4f4c9f945834d0
Coder1998Man <coder1998man@gmail.com>
2024-01-30 15:04:21 +0330

    modified for serve the static files

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import projects, project, create_project, update_project, delete_project
 
 urlpatterns = [
     path('', projects, name='home'),
     path('create_project_form/', create_project, name='create_project'),
-    path('project/<uuid:pk>/', project, name='project'),
+    path('project/<str:pk>/', project, name='project'),
     path('update_project/<str:pk>/', update_project, name='update_project'),
     path('delete_project/<str:pk>/', delete_project, name='delete_project'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
