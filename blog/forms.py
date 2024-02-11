12d09abe4aa11c8712032fc73009cc4debd7fa4a
Coder1998Man <155251879+Coder1998Man@users.noreply.github.com>
2024-02-11 18:19:22 +0000

    added  check box whit widgets

--- blog/forms.py
+++ blog/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, widgets
 from .models import Project
 
 
@@ -6,3 +6,12 @@ class ProjectForm(ModelForm):
     class Meta:
         model = Project
         fields = ['title', 'featured_image', 'description', 'demo_link', 'source_link', 'tags']
+        widgets = {
+            'tags': widgets.CheckboxSelectMultiple(),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
