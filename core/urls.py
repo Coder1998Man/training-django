effcf30f3ba1163b3baa79d53da4035934d97eb0
Coder1998Man <155251879+Coder1998Man@users.noreply.github.com>
2024-02-14 00:31:37 +0000

    edit path of accounts and blog incloude path

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
-    path('accounts/', include('accounts.urls')),
+    path('projects', include('blog.urls')),
+    path('', include('accounts.urls')),
 ]
