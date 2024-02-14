b10edc53a215641f6b863bb1ede9a37c71ca6e66
Coder1998Man <155251879+Coder1998Man@users.noreply.github.com>
2024-02-14 00:30:26 +0000

    change home page to profiles

--- blog/urls.py
+++ blog/urls.py
@@ -6,7 +6,7 @@ from .views import projects, project, create_project, update_project, delete_pro
 urlpatterns = [
     path('', projects, name='home'),
     path('create_project_form/', create_project, name='create_project'),
-    path('project/<str:pk>/', project, name='project'),
+    path('/project/<str:pk>/', project, name='project'),
     path('update_project/<str:pk>/', update_project, name='update_project'),
     path('delete_project/<str:pk>/', delete_project, name='delete_project'),
 ]
