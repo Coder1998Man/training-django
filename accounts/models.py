99b09b46192aa52fde017b78b63b90eaac259ea6
Coder1998Man <155251879+Coder1998Man@users.noreply.github.com>
2024-02-15 00:31:55 +0000

    defined profile model class

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+import uuid
 
 
 class CustomUser(AbstractUser):
@@ -7,3 +8,25 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.email
+
+
+
+class profile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    email = models.EmailField(max_length=200, null=True, blank=True)
+    username = models.CharField(max_length=200, null=True, blank=True)
+    short_intro = models.CharField(max_length=200, null=True, blank=True)
+    bio = models.TextField(null=True, blank=True)
+    profile_image = models.ImageField(null=True, blank=True, upload_to='profiles/', default='user-default.png')
+    social_github = models.CharField(max_length=200, null=True, blank=True)
+    social_twitter = models.CharField(max_length=200, null=True, blank=True)
+    social_linkedin = models.CharField(max_length=200, null=True, blank=True)
+    social_youtube = models.CharField(max_length=200, null=True, blank=True)
+    social_website = models.CharField(max_length=200, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.user.username)
+    
