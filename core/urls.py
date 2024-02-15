81a5c8e89048043cec285548e6506a77fba26096
Coder1998Man <155251879+Coder1998Man@users.noreply.github.com>
2024-02-15 00:36:11 +0000

    edited touting

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('projects', include('blog.urls')),
+    path('projects/', include('blog.urls')),
     path('', include('accounts.urls')),
 ]
