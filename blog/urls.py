eaef92fce851ad2a02ba1f5ef034070d1564af85
Coder1998Man <155251879+Coder1998Man@users.noreply.github.com>
2024-02-16 03:33:12 +0000

    some fix

--- blog/urls.py
+++ blog/urls.py
@@ -4,11 +4,12 @@ from django.conf.urls.static import static
 from .views import projects, project, create_project, update_project, delete_project
 
 urlpatterns = [
-    path('', projects, name='home'),
+    path('', projects, name='projects'),
     path('create_project_form/', create_project, name='create_project'),
     path('project/<str:pk>/', project, name='project'),
     path('update_project/<str:pk>/', update_project, name='update_project'),
     path('delete_project/<str:pk>/', delete_project, name='delete_project'),
+
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
