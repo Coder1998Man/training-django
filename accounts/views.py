9fbc747e7f6b5754d0dbb8eee5acd4424af0d687
coder1998man <coder1998man@gmail.com>
2024-02-25 23:19:50 +0330

    writen user profile view

--- accounts/views.py
+++ accounts/views.py
@@ -8,3 +8,15 @@ def profiles(request):
         'profiles': profiles
     }
     return render(request, 'accounts/profiles.html', context)
+
+
+def userProfile(request, pk):
+    user_profile = profile.objects.get(id=pk)
+    top_skills = user_profile.skill_set.exclude(description__exact="")
+    other_skills = user_profile.skill_set.filter(description="")
+    context = {
+        'user_profile': user_profile,
+        'top_skills': top_skills,
+        'other_skills': other_skills
+    }
+    return render(request, 'accounts/user-profile.html', context)
